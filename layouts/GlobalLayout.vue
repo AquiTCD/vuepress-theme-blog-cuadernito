0bf63f0661aca73332e41c3192909da817170704
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2019-06-23 21:56:14 +0900

    feat(GlobalLayout): add Header functions

--- layouts/GlobalLayout.vue
+++ layouts/GlobalLayout.vue
@@ -1,33 +1,125 @@
-<template>
-  <div id="vuperess-theme-blog__global-layout">
-    <Header/>
-    <div class="content-wrapper">
-      <DefaultGlobalLayout/>
-    </div>
-    <Footer/>
-  </div>
+<template lang="pug">
+  #vuperess-theme-blog__global-layout.l-body.theme-container(
+    :class="pageClasses"
+    @touchstart="onTouchStart"
+    @touchend="onTouchEnd"
+  )
+    Header(:should-show-navbar="shouldShowNavbar" :sidebar-items="sidebarItems" @toggle-sidebar="toggleSidebar")
+    .content-wrapper
+      DefaultGlobalLayout
+    Footer
 </template>
 
 <script>
-  import GlobalLayout from '@app/components/GlobalLayout.vue'
-  import Header from '@theme/components/Header.vue'
-  import MobileHeader from '@theme/components/MobileHeader.vue'
-  import Footer from '@theme/components/Footer.vue'
+import GlobalLayout from '@app/components/GlobalLayout.vue'
+import Header from '@theme/components/Header.vue'
+import MobileHeader from '@theme/components/MobileHeader.vue'
+import Footer from '@theme/components/Footer.vue'
+import 'ress/dist/ress.min.css'
+import { resolveSidebarItems } from '@theme/components/util'
 
-  export default {
-    components: {
-      DefaultGlobalLayout: GlobalLayout,
-      Header,
-      MobileHeader,
-      Footer
+export default {
+  components: {
+    DefaultGlobalLayout: GlobalLayout,
+    Header,
+    MobileHeader,
+    Footer,
+  },
+  data() {
+    return {
+      isSidebarOpen: false,
+    }
+  },
+  computed: {
+    // isIndex() {
+    //   return !!(
+    //     this.$page.frontmatter.home ||
+    //     this.$pagination.paginationIndex ||
+    //     this.$category ||
+    //     this.$tag
+    //   )
+    // },
+    shouldShowNavbar() {
+      const { themeConfig } = this.$site
+      const { frontmatter } = this.$page
+      if (frontmatter.navbar === false || themeConfig.navbar === false) {
+        return false
+      }
+      return (
+        this.$title ||
+        themeConfig.logo ||
+        themeConfig.repo ||
+        themeConfig.nav ||
+        this.$themeLocaleConfig.nav
+      )
     },
-  }
+
+    shouldShowSidebar() {
+      const { frontmatter } = this.$page
+      return (
+        !frontmatter.home &&
+        frontmatter.layout === 'Post' &&
+        frontmatter.sidebar !== false &&
+        this.sidebarItems.length
+      )
+    },
+
+    sidebarItems() {
+      return resolveSidebarItems(
+        this.$page,
+        this.$page.regularPath,
+        this.$site,
+        this.$localePath
+      )
+    },
+
+    pageClasses() {
+      const userPageClass = this.$page.frontmatter.pageClass
+      return [
+        {
+          'no-navbar': !this.shouldShowNavbar,
+          'sidebar-open': this.isSidebarOpen,
+          'no-sidebar': !this.shouldShowSidebar,
+        },
+        userPageClass,
+      ]
+    },
+  },
+
+  mounted() {
+    this.$router.afterEach(() => {
+      this.isSidebarOpen = false
+    })
+  },
+
+  methods: {
+    toggleSidebar(to) {
+      console.log(to)
+
+      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
+    },
+
+    // side swipe
+    onTouchStart(e) {
+      this.touchStart = {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY,
+      }
+    },
+
+    onTouchEnd(e) {
+      const dx = e.changedTouches[0].clientX - this.touchStart.x
+      const dy = e.changedTouches[0].clientY - this.touchStart.y
+      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+        if (dx > 0 && this.touchStart.x <= 80) {
+          this.toggleSidebar(true)
+        } else {
+          this.toggleSidebar(false)
+        }
+      }
+    },
+  },
+}
 </script>
 
-<style lang="stylus">
-  .content-wrapper
-    padding 160px 15px 80px 15px
-    min-height calc(100vh - 80px - 60px - 160px)
-    max-width 740px
-    margin 0 auto
-</style>
+<style lang="stylus"></style>
